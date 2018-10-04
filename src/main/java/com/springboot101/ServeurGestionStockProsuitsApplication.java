3f82d73f4cbc8bb2d92063fd18ca1cad409a2c79
YahyaDadda <yahyadadda@gmail.com>
2018-10-04 20:59:39 +0100

    first commit Server

---
+++ src/main/java/com/springboot101/ServeurGestionStockProsuitsApplication.java
@@ -0,0 +1,12 @@
+package com.springboot101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionStockProsuitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionStockProsuitsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/springboot101/ServeurGestionStockProsuitsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springboot101;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServeurGestionStockProsuitsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
